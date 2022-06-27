fdb5ec1226c5eb0c8038045e23464edc65ac818f
Álvaro Bartolomé <alvarob96@usal.es>
2022-06-27 18:32:58 +0000

    📦 Create basic package structure

---
+++ src/package/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 Alvaro Bartolome, alvarobartt @ GitHub
+# See LICENSE for details.
+
+"""`package `: Python package description."""
+
+__author__ = "Alvaro Bartolome, alvarobartt @ GitHub"
+__version__ = "0.0.1"
