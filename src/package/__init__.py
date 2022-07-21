eb405e7ce6c7640919d63dfba181214dc1808e89
Alvaro Bartolome <alvarob96@usal.es>
2022-07-21 08:50:48 +0000

    🚚 Replace name/surname with placeholders

--- src/package/__init__.py
+++ src/package/__init__.py
@@ -1,7 +1,7 @@
-# Copyright 2022 Alvaro Bartolome, alvarobartt @ GitHub
+# Copyright 2022 Name Surname, username @ GitHub
 # See LICENSE for details.
 
 """`package `: Python package description."""
 
-__author__ = "Alvaro Bartolome, alvarobartt @ GitHub"
+__author__ = "Name Surname, username @ GitHub"
 __version__ = "0.0.1"
